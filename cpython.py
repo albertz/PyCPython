705ca243f339f4faa2c85945d8ef404461b3f27b
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-12 02:06:09 +0200

    make 'import cpython' somewhat sane

--- cpython.py
+++ cpython.py
@@ -7,67 +7,76 @@ import better_exchook
 better_exchook.install()
 
 import sys, os, os.path
-MyDir = os.path.dirname(sys.argv[0])
+if __name__ == '__main__':
+	MyDir = os.path.dirname(sys.argv[0])
+else:
+	MyDir = "."
 
 CPythonDir = MyDir + "/Python-2.7.1"
 
 import cparser
-state = cparser.State()
-state.autoSetupSystemMacros()
 
-def findIncludeFullFilename(filename, local):
-	fullfn = CPythonDir + "/Include/" + filename
-	if os.path.exists(fullfn): return fullfn
-	return filename
-
-state.findIncludeFullFilename = findIncludeFullFilename
-
-def readLocalInclude(state, filename):
-	if filename == "pyconfig.h":
-		def reader():
-			import ctypes
-			sizeofMacro = lambda t: cparser.Macro(rightside=str(ctypes.sizeof(t)))
-			state.macros["SIZEOF_SHORT"] = sizeofMacro(ctypes.c_short)
-			state.macros["SIZEOF_INT"] = sizeofMacro(ctypes.c_int)
-			state.macros["SIZEOF_LONG"] = sizeofMacro(ctypes.c_long)
-			state.macros["SIZEOF_LONG_LONG"] = sizeofMacro(ctypes.c_longlong)
-			state.macros["SIZEOF_DOUBLE"] = sizeofMacro(ctypes.c_double)
-			state.macros["SIZEOF_FLOAT"] = sizeofMacro(ctypes.c_float)
-			state.macros["SIZEOF_VOID_P"] = sizeofMacro(ctypes.c_void_p)
-			state.macros["SIZEOF_SIZE_T"] = sizeofMacro(ctypes.c_size_t)
-			state.macros["SIZEOF_UINTPTR_T"] = sizeofMacro(ctypes.POINTER(ctypes.c_uint))
-			state.macros["SIZEOF_PTHREAD_T"] = state.macros["SIZEOF_LONG"]
-			state.macros["SIZEOF_PID_T"] = state.macros["SIZEOF_INT"]
-			state.macros["SIZEOF_TIME_T"] = state.macros["SIZEOF_LONG"]
-			state.macros["SIZEOF__BOOL"] = cparser.Macro(rightside="1")
-			return
-			yield None # make it a generator
-		return reader(), None
-	return cparser.State.readLocalInclude(state, filename)
-
-state.readLocalInclude = lambda fn: readLocalInclude(state, fn)
-
-state.autoSetupGlobalIncludeWrappers()
-
-state = cparser.parse(CPythonDir + "/Modules/main.c", state)
-
-print "erros so far:"
-for m in state._errors:
-	print m
-
-for f in state.contentlist:
-	if not isinstance(f, cparser.CFunc): continue
-	if not f.body: continue
+def prepareState():
+	state = cparser.State()
+	state.autoSetupSystemMacros()
 	
-	print
-	print "parsed content of " + str(f) + ":"
-	for c in f.body.contentlist:
-		print c
+	def findIncludeFullFilename(filename, local):
+		fullfn = CPythonDir + "/Include/" + filename
+		if os.path.exists(fullfn): return fullfn
+		return filename
+	
+	state.findIncludeFullFilename = findIncludeFullFilename
+	
+	def readLocalInclude(state, filename):
+		if filename == "pyconfig.h":
+			def reader():
+				import ctypes
+				sizeofMacro = lambda t: cparser.Macro(rightside=str(ctypes.sizeof(t)))
+				state.macros["SIZEOF_SHORT"] = sizeofMacro(ctypes.c_short)
+				state.macros["SIZEOF_INT"] = sizeofMacro(ctypes.c_int)
+				state.macros["SIZEOF_LONG"] = sizeofMacro(ctypes.c_long)
+				state.macros["SIZEOF_LONG_LONG"] = sizeofMacro(ctypes.c_longlong)
+				state.macros["SIZEOF_DOUBLE"] = sizeofMacro(ctypes.c_double)
+				state.macros["SIZEOF_FLOAT"] = sizeofMacro(ctypes.c_float)
+				state.macros["SIZEOF_VOID_P"] = sizeofMacro(ctypes.c_void_p)
+				state.macros["SIZEOF_SIZE_T"] = sizeofMacro(ctypes.c_size_t)
+				state.macros["SIZEOF_UINTPTR_T"] = sizeofMacro(ctypes.POINTER(ctypes.c_uint))
+				state.macros["SIZEOF_PTHREAD_T"] = state.macros["SIZEOF_LONG"]
+				state.macros["SIZEOF_PID_T"] = state.macros["SIZEOF_INT"]
+				state.macros["SIZEOF_TIME_T"] = state.macros["SIZEOF_LONG"]
+				state.macros["SIZEOF__BOOL"] = cparser.Macro(rightside="1")
+				return
+				yield None # make it a generator
+			return reader(), None
+		return cparser.State.readLocalInclude(state, filename)
+	
+	state.readLocalInclude = lambda fn: readLocalInclude(state, fn)
+	
+	state.autoSetupGlobalIncludeWrappers()
+	
+	return state
 
-import cparser.interpreter
+state = prepareState()
+cparser.parse(CPythonDir + "/Modules/main.c", state)
 
-r = cparser.interpreter.Interpreter()
-r.register(state)
-
-r.runFunc("Py_Main", len(sys.argv), sys.argv)
+if __name__ == '__main__':
+	print "erros so far:"
+	for m in state._errors:
+		print m
+	
+	for f in state.contentlist:
+		if not isinstance(f, cparser.CFunc): continue
+		if not f.body: continue
+		
+		print
+		print "parsed content of " + str(f) + ":"
+		for c in f.body.contentlist:
+			print c
+	
+	import cparser.interpreter
+	
+	r = cparser.interpreter.Interpreter()
+	r.register(state)
+	
+	r.runFunc("Py_Main", len(sys.argv), sys.argv)
 
