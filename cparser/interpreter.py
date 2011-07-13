25fa764a6c406abadffe89f19cd66370be776abd
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-14 01:46:33 +0200

    updated cparser

--- cparser/interpreter.py
+++ cparser/interpreter.py
@@ -35,7 +35,8 @@ def iterIdWithPostfixes(name):
 	for postfix in iterIdentifierNames():
 		yield name + "_" + postfix
 
-PyReservedNames = set(dir(__builtins__))
+import keyword
+PyReservedNames = set(dir(sys.modules["__builtin__"]) + keyword.kwlist + ["ctypes"])
 
 def isValidVarName(name):
 	return name not in PyReservedNames
@@ -86,9 +87,31 @@ class FuncEnv:
 		scope = self.scopeStack.pop()
 		scope.finishMe()
 
-def getAstNodeForVarType(varDecl):
-	# TODO
-	pass
+def getAstNodeForCTypesBasicType(t):
+	import inspect
+	if t is None: return None
+	if t is CVoidType: return None
+	if not inspect.isclass(t) and isinstance(t, CVoidType): return None
+	if issubclass(t, CVoidType): return None
+	a = ast.Attribute()
+	a.value = ast.Name(id="ctypes")
+	a.attr = t.__name__
+	return a
+
+def getAstNodeForVarType(t):
+	if isinstance(t, CBuiltinType):
+		return getAstNodeForCTypesBasicType(t.builtinType)
+	elif isinstance(t, CStdIntType):
+		return getAstNodeForCTypesBasicType(State.StdIntTypes[t.name])
+	elif isinstance(t, CPointerType):
+		a = ast.Attribute()
+		a.value = ast.Name(id="ctypes")
+		a.attr = "POINTER"
+		return makeAstNodeCall(a, getAstNodeForVarType(t.pointerOf))
+	elif isinstance(t, CTypedefType):
+		return ast.Name(id=t.name)
+	else:
+		assert False, "cannot handle " + str(t)
 
 def makeAstNodeCall(func, *args):
 	if not isinstance(func, ast.AST):
@@ -104,11 +127,12 @@ class FuncCodeblockScope:
 		varName = self.funcEnv._registerNewVar(varName, varDecl)
 		assert varName is not None
 		self.varNames.add(varName)
-		a = ast.Assign()
-		a.targets = [ast.Name(id=varName)]
-		varTypeNode = getAstNodeForVarType(varDecl)
-		a.value = makeAstNodeCall(varTypeNode)
-		self.funcEnv.astNode.body.append(a)
+		if isinstance(varDecl, CVarDecl): # it could also be a CFuncArgDecl or so. only assing if CVarDecl
+			a = ast.Assign()
+			a.targets = [ast.Name(id=varName)]
+			varTypeNode = getAstNodeForVarType(varDecl.type)
+			a.value = makeAstNodeCall(varTypeNode)
+			self.funcEnv.astNode.body.append(a)
 		return varName
 	def _astForDeleteVar(self, varName):
 		assert varName is not None
