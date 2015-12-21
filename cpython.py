e61e87f181477a62fcbacfede3196ccd0f1056f8
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-12-21 17:59:23 +0100

    dictobject

--- cpython.py
+++ cpython.py
@@ -98,6 +98,7 @@ def main(argv):
 	cparser.parse(CPythonDir + "/Objects/object.c", state) # _Py_ReadyTypes etc
 	cparser.parse(CPythonDir + "/Objects/typeobject.c", state) # PyType_Ready
 	cparser.parse(CPythonDir + "/Objects/tupleobject.c", state) # PyTuple_New
+	cparser.parse(CPythonDir + "/Objects/dictobject.c", state)  # PyDict_New
 	cparser.parse(CPythonDir + "/Objects/obmalloc.c", state) # PyObject_Free
 	cparser.parse(CPythonDir + "/Modules/gcmodule.c", state) # _PyObject_GC_NewVar
 	cparser.parse(CPythonDir + "/Include/structmember.h", state) # struct PyMemberDef. just for now to avoid errors :)
