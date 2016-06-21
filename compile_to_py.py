eea1a7c6729c84f98675e09da95a34a90d08c494
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-06-21 22:29:56 +0200

    static compile

--- compile_to_py.py
+++ compile_to_py.py
@@ -51,8 +51,65 @@ def main(argv):
 	f.write("intp.setupStatic()\n")
 	f.write("helpers = intp.helpers\n")
 	f.write("ctypes_wrapped = intp.ctypes_wrapped\n")
-	# TODO: structs, unions, values
-	f.write("\n\nclass g:\n")
+	f.write("\n\n")
+
+	f.write("class structs:\n")
+	last_log_time = time.time()
+	for i, content in enumerate(state.contentlist):
+		if time.time() - last_log_time > 2.0:
+			last_log_time = time.time()
+			perc_compl = 100.0 * i / len(state.contentlist)
+			cur_content_s = "%s %s" % (content.__class__.__name__,
+									   (getattr(content, "name", None) or "<noname>"))
+			cur_file_s = getattr(content, "defPos", "<unknown source>")
+			print "Compile structs... (%.0f%%) (%s) (%s)" % (perc_compl, cur_content_s, cur_file_s)
+		if not content.name:
+			continue
+		if not isinstance(content, cparser.CStruct):
+			continue
+		if cparser.isExternDecl(content):
+			content = state.getResolvedDecl(content)
+			if cparser.isExternDecl(content):
+				# Dummy placeholder.
+				f.write("    cls %s: 'Dummy extern struct declaration'\n" % content.name)
+				continue
+			else:
+				# We have a full declaration available.
+				continue  # we will write it later
+		# see _getCTypeStruct for reference
+		# TODO. not simple.
+		f.write("    cls %s: 'Dummy'\n" % content.name)  # Dummy for now
+	f.write("\n\n")
+
+	f.write("class unions:\n")
+	for i, content in enumerate(state.contentlist):
+		if time.time() - last_log_time > 2.0:
+			last_log_time = time.time()
+			perc_compl = 100.0 * i / len(state.contentlist)
+			cur_content_s = "%s %s" % (content.__class__.__name__,
+									   (getattr(content, "name", None) or "<noname>"))
+			cur_file_s = getattr(content, "defPos", "<unknown source>")
+			print "Compile unions... (%.0f%%) (%s) (%s)" % (perc_compl, cur_content_s, cur_file_s)
+		if not content.name:
+			continue
+		if not isinstance(content, cparser.CUnion):
+			continue
+		if cparser.isExternDecl(content):
+			content = state.getResolvedDecl(content)
+			if cparser.isExternDecl(content):
+				# Dummy placeholder.
+				f.write("    cls %s: 'Dummy extern union declaration'\n" % content.name)
+				continue
+			else:
+				# We have a full declaration available.
+				continue  # we will write it later
+		# see _getCTypeStruct for reference
+		# TODO. not simple. see above for structs
+		f.write("    cls %s: 'Dummy'\n" % content.name)  # Dummy for now
+	f.write("\n\n")
+
+	f.write("class g:\n")
+	g_names = set()
 	last_log_time = time.time()
 	count = count_incomplete = 0
 	for i, content in enumerate(state.contentlist):
@@ -63,6 +120,8 @@ def main(argv):
 									   (getattr(content, "name", None) or "<noname>"))
 			cur_file_s = getattr(content, "defPos", "<unknown source>")
 			print "Compile... (%.0f%%) (%s) (%s)" % (perc_compl, cur_content_s, cur_file_s)
+		if isinstance(content, (cparser.CStruct, cparser.CUnion)):
+			continue  # Handled in the other loops.
 		try:
 			if cparser.isExternDecl(content):
 				content = state.getResolvedDecl(content)
@@ -73,6 +132,11 @@ def main(argv):
 					# We have a full declaration available.
 					continue  # we will write it later
 			count += 1
+			if content.name:
+				assert content.name not in g_names, "%r defined twice" % content.name
+				g_names.add(content.name)
+			else:
+				continue
 			if isinstance(content, cparser.CFunc):
 				funcEnv = interpreter._translateFuncToPyAst(content, noBodyMode="code-with-exception")
 				pyAst = funcEnv.astNode
@@ -80,16 +144,24 @@ def main(argv):
 				pyAst.decorator_list.append(ast.Name(id="staticmethod", ctx=ast.Load()))
 				Unparser(pyAst, indent=1, file=f)
 			elif isinstance(content, (cparser.CStruct, cparser.CUnion)):
-				# see _getCTypeStruct for reference
-				pass  # TODO. not simple.
+				pass  # Handled in the other loops.
+			elif isinstance(content, cparser.CTypedef):
+				f.write("    %s = 'Dummy typedef'\n" % content.name)  # TODO
+			elif isinstance(content, cparser.CVarDecl):
+				f.write("    %s = 'Dummy vardecl'\n" % content.name)  # TODO
+			elif isinstance(content, cparser.CEnum):
+				int_type_name = content.getMinCIntType()
+				f.write("    %s = ctypes.%s\n" % (content.name, int_type_name))
 			else:
 				raise Exception("unexpected content type: %s" % type(content))
-		except Exception:
+		except Exception as exc:
 			print "!!! Exception while compiling %r" % content
+			if content.name:
+				f.write("   %s = 'Compile exception ' %r\n" % (content.name, str(exc)))
 			sys.excepthook(*sys.exc_info())
 			# We continue...
+	f.write("\n\n")
 
-	f.write("\n\n\n")
 	f.write("class values:\n")
 	def maybe_add_wrap_value(container_name, var_name, var):
 		if not isinstance(var, cparser.CWrapValue): return
@@ -106,14 +178,15 @@ def main(argv):
 		maybe_add_wrap_value("vars", varname, var)
 	for varname, var in sorted(state.funcs.items()):
 		maybe_add_wrap_value("funcs", varname, var)
+	f.write("\n\n")
 
-	f.write("\n\n\n")
 	f.write("if __name__ == '__main__':\n")
 	f.write("    better_exchook.install()\n")
 	f.write("    g.Py_Main(ctypes_wrapped.c_int(len(sys.argv)),\n"
 			"              (ctypes.POINTER(ctypes_wrapped.c_char) * (len(sys.argv) + 1))(\n"
 			"               *[ctypes.cast(intp._make_string(arg), ctypes.POINTER(ctypes_wrapped.c_char))\n"
-			"                 for arg in sys.argv]))\n\n")
+			"                 for arg in sys.argv]))\n")
+	f.write("\n")
 	f.close()
 
 	print "Done."
